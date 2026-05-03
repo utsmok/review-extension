666986af8f4c80e4f08dca6a4b699703fd8d8170
utsmok <s.mok@utwente.nl>
2026-05-03 20:26:53 +0200

    chore(assets): add extension icons and wire into manifest

--- wxt.config.ts
+++ wxt.config.ts
@@ -12,6 +12,14 @@ export default defineConfig({
     },
     action: {
       default_title: "Open TRUST Review",
+      default_icon: {
+        "16": "icon-16.png",
+        "19": "icon-19.png",
+        "32": "icon-32.png",
+        "38": "icon-38.png",
+        "48": "icon-48.png",
+        "128": "icon-128.png",
+      },
     },
   },
   vite: () => ({
